c145545bc74c77ea6b66fbf904ffa80bb42af700
Guille-B <guillebau2012@gmail.com>
2024-11-15 17:08:07 -0300

    cosas

--- inicio.js
+++ inicio.js
@@ -1,48 +1,4 @@
-/*fetch('./nav/nav.html')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.text();
-            })
-            .then(data => {
-                document.getElementById('navbar').innerHTML = data;
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            });
-
-        // Cargar el archivo footer.html
-        fetch('./footer/footer.html')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.text();
-            })
-            .then(data => {
-                document.getElementById('footer').innerHTML = data;
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            });
-
-            //dddddddddddddddddddddddd
-            fetch('./home/home.html')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.text();
-            })
-            .then(data => {
-                document.getElementById('home').innerHTML = data;
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            });*/
-
-            function loadHTMLWithScript(htmlPath, scriptPath, elementId) {
+function loadHTMLWithScript(htmlPath, scriptPath, elementId) {
                 fetch(htmlPath)
                     .then(response => {
                         if (!response.ok) {
@@ -77,4 +33,5 @@
     
             // Cargar home con su script
             loadHTMLWithScript('./home/home.html', './home/home.js', 'home');
-     
+
+            
